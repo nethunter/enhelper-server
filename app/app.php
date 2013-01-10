44276582b673cdbd0451a2e998011ff49309f71a
David Tabachnikov <nethunter@studiosh.com>
2013-01-11 01:01:49 +0200

    Initial commit

---
+++ app/app.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+$app = new Silex\Application();
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+return $app;
